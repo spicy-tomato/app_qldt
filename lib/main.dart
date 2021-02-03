8b60ed3882d575f8a5cfe3a28dc69cd82dd5131e
vanhuy166 <saikhe166@gmail.com>
2021-02-03 16:22:38 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,24 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'screens/home/home.dart';
+import 'screens/login/login.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Mt App",
-    home: MyScaffold(),
-  ));
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+      },
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('vi', ''),
+      ],
+    ),
+  );
 }
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar([this.title]);
-  final Widget title;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 70.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.lightBlue),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation menu",
-            onPressed: null,
-
-          ),
-
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "search",
-            onPressed: null,
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            Text(
-              'Trang Chủ',
-              // ignore: deprecated_member_use
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-
-          Container(
-            height: 500.0,
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            decoration: BoxDecoration(color: Colors.lightGreen),
-
-            child: Column(
-              children: <Widget>[
-                Image.asset('img/nxb2.jpg',width: 400.0,height: 200.0,),
-                Column (
-                  children: [
-                    Text('Hello world'),
-                  ],
-                ),
-              ],
-
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-}
-
-
