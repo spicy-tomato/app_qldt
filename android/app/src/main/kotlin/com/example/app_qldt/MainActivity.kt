e7af53a4f3f2bf79084179155542e18a9d3daf93
An Nguyễn <snowflower1408@gmail.com>
2021-03-01 20:48:27 +0700

    Config manifest files

--- android/app/src/main/kotlin/com/example/app_qldt/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app_qldt/MainActivity.kt
@@ -1,6 +1,33 @@
 package com.example.app_qldt
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.content.ContentResolver
+import java.util.*
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "dexterx.dev/flutter_local_notifications_example").setMethodCallHandler { call, result ->
+            if ("drawableToUri" == call.method) {
+                val resourceId = this@MainActivity.resources.getIdentifier(call.arguments as String, "drawable", this@MainActivity.packageName)
+                result.success(resourceToUriString(this@MainActivity.applicationContext, resourceId))
+            }
+            if ("getAlarmUri" == call.method) {
+                result.success(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString())
+            }
+            if ("getTimeZoneName" == call.method) {
+                result.success(TimeZone.getDefault().id)
+            }
+        }
+    }
+
+    private fun resourceToUriString(context: Context, resId: Int): String? {
+        return (ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                + context.resources.getResourcePackageName(resId)
+                + "/"
+                + context.resources.getResourceTypeName(resId)
+                + "/"
+                + context.resources.getResourceEntryName(resId))
+    }
 }
