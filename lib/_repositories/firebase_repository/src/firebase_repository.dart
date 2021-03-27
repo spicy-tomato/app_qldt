0c874fe3a45df3b8ea2e84b145968b6a36fe0170
An Nguyen <annguyen.it.utc@gmail.com>
2021-03-27 22:16:27 +0700

    Make notification display in app

--- lib/_repositories/firebase_repository/src/firebase_repository.dart
+++ lib/_repositories/firebase_repository/src/firebase_repository.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseRepository {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
@@ -17,9 +18,54 @@ class FirebaseRepository {
       sound: true,
     );
 
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.max,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('mipmap/ic_launcher');
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
       print(message.data);
       print(message.notification);
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channel.description,
+              icon: android.smallIcon,
+              priority: Priority.high,
+              importance: Importance.max,
+              showWhen: false,
+              // other properties...
+            ),
+          ),
+        );
+      }
     });
   }
 
