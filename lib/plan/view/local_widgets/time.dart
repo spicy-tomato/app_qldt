e492d6f824c410fdc827f90c7391a9d955b84903
An Nguyen <annguyen.it.utc@gmail.com>
2021-05-04 22:21:41 +0700

    Implement BLoC for plan page time

--- lib/plan/view/local_widgets/time.dart
+++ lib/plan/view/local_widgets/time.dart
@@ -1,4 +1,6 @@
+import 'package:app_qldt/plan/bloc/plan_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 import 'shared/shared.dart';
@@ -14,60 +16,40 @@ class _PlanPageTimeState extends State<PlanPageTime> {
 
   @override
   Widget build(BuildContext context) {
-    return CommonPadding(
-      child: Column(
-        children: <Widget>[
-          SwitcherTile(),
-          DatePicker(dateTime: from),
-          DatePicker(dateTime: to),
-          Repeat(),
-        ],
-      ),
+    return Column(
+      children: <Widget>[
+        SwitcherTile(),
+        FromDatePicker(),
+        ToDatePicker(),
+        Repeat(),
+      ],
     );
   }
 }
 
-//#region Switcher
+//#region SwitcherTile
 
-class SwitcherTile extends StatelessWidget {
+class SwitcherTile extends StatefulWidget {
+  _SwitcherTileState createState() => _SwitcherTileState();
+}
+
+class _SwitcherTileState extends State<SwitcherTile> {
   @override
   Widget build(BuildContext context) {
-    return PlanPageCustomListTile(
-      leading: Icon(Icons.access_time_rounded),
-      title: Text('Cả ngày', style: PlanPageConstant.of(context).textFieldStyle),
-      trailing: Switcher(),
+    return BlocBuilder<PlanBloc, PlanState>(
+      buildWhen: (previous, current) => previous.isAllDay != current.isAllDay,
+      builder: (context, state) {
+        return PlanPageCustomListTile(
+          leading: const Icon(Icons.access_time_rounded),
+          title: Text('Cả ngày', style: PlanPageConstant.of(context).textFieldStyle),
+          trailing: Switch(
+            value: state.isAllDay,
+            onChanged: (_) => context.read<PlanBloc>().add(PlanIsAllDayChanged()),
+          ),
+          onTap: () => context.read<PlanBloc>().add(PlanIsAllDayChanged()),
+        );
+      },
     );
-  }
-}
-
-class Switcher extends StatefulWidget {
-  final bool? initState;
-
-  Switcher({this.initState});
-
-  @override
-  _SwitcherState createState() => _SwitcherState(initState);
-}
-
-class _SwitcherState extends State<Switcher> {
-  late bool isOn;
-
-  _SwitcherState(bool? isOn) {
-    this.isOn = isOn ?? false;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Switch(
-      value: isOn,
-      onChanged: toggleSwitch,
-    );
-  }
-
-  void toggleSwitch(_) {
-    setState(() {
-      isOn = !isOn;
-    });
   }
 }
 
@@ -75,74 +57,167 @@ class _SwitcherState extends State<Switcher> {
 
 //#region DatePicker
 
-class DatePicker extends StatefulWidget {
-  final DateTime dateTime;
+extension DateTimeExtesion on DateTime {
+  bool isSameDay(DateTime other) => year == other.year && month == other.month && day == other.day;
 
-  const DatePicker({
-    Key? key,
-    required this.dateTime,
-  }) : super(key: key);
-
-  @override
-  _DatePickerState createState() => _DatePickerState(dateTime);
+  bool isSameTime(DateTime other) =>
+      this.isSameDay(other) && hour == other.hour && minute == other.minute;
 }
 
-class _DatePickerState extends State<DatePicker> {
-  DateTime date;
-  late TimeOfDay timeOfDay;
-
-  _DatePickerState(this.date) {
-    timeOfDay = TimeOfDay.fromDateTime(date);
-  }
-
+class FromDatePicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PlanPageCustomListTile(
-      title: TextButton(
-        style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.all(0))),
-        onPressed: () async {
-          DateTime? newDate = await showDatePicker(
-            context: context,
-            firstDate: DateTime.now(),
-            lastDate: DateTime(2100),
-            initialDate: date,
-          );
-
-          if (newDate != null) {
-            setState(() {
-              date = newDate;
-            });
-          }
-
-          return Future.value(false);
-        },
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-              "${DateFormat('E, d MMMM, y', Localizations.localeOf(context).languageCode).format(date)}",
-              style: PlanPageConstant.of(context).textFieldStyle),
+      title: Align(
+        alignment: Alignment.centerLeft,
+        child: BlocBuilder<PlanBloc, PlanState>(
+          buildWhen: (previous, current) => !previous.fromDay.isSameDay(current.fromDay),
+          builder: (context, state) {
+            return TextButton(
+              style: ButtonStyle(padding: MaterialStateProperty.all(const EdgeInsets.all(0))),
+              onPressed: () => _chooseDay(context, state),
+              child: Text(
+                  "${DateFormat('E, d MMMM, y', Localizations.localeOf(context).languageCode).format(state.fromDay)}",
+                  style: PlanPageConstant.of(context).textFieldStyle),
+            );
+          },
         ),
       ),
-      trailing: TextButton(
-        style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.only(right: 10))),
-        child: Text(
-          "${DateFormat.Hm().format(date)}",
-          style: PlanPageConstant.of(context).textFieldStyle,
+      trailing: Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: BlocBuilder<PlanBloc, PlanState>(
+          buildWhen: (previous, current) => !previous.fromDay.isSameTime(current.fromDay),
+          builder: (context, state) {
+            return TextButton(
+              onPressed: () => _chooseTime(context, state),
+              child: Text(
+                "${DateFormat.Hm().format(state.fromDay)}",
+                style: PlanPageConstant.of(context).textFieldStyle,
+              ),
+            );
+          },
         ),
-        onPressed: () async {
-          TimeOfDay? newTimeOfDay = await showTimePicker(context: context, initialTime: timeOfDay);
-
-          if (newTimeOfDay != null) {
-            setState(() {
-              timeOfDay = newTimeOfDay;
-            });
-          }
-
-          return Future.value(false);
-        },
       ),
     );
   }
+
+  final Function _chooseDay = (BuildContext context, PlanState state) async {
+    DateTime? newDate = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+      initialDate: state.fromDay,
+    );
+
+    if (newDate != null) {
+      newDate = DateTime(
+        newDate.year,
+        newDate.month,
+        newDate.day,
+        state.fromDay.hour,
+        state.fromDay.minute,
+      );
+
+      context.read<PlanBloc>().add(PlanFromDateChanged(newDate));
+    }
+  };
+
+  final Function _chooseTime = (BuildContext context, PlanState state) async {
+    TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(state.fromDay),
+    );
+
+    if (newTime != null) {
+      DateTime newDate = DateTime(
+        state.fromDay.year,
+        state.fromDay.month,
+        state.fromDay.day,
+        newTime.hour,
+        newTime.minute,
+      );
+
+      context.read<PlanBloc>().add(PlanFromDateChanged(newDate));
+    }
+  };
+}
+
+class ToDatePicker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PlanPageCustomListTile(
+      title: Align(
+        alignment: Alignment.centerLeft,
+        child: BlocBuilder<PlanBloc, PlanState>(
+          buildWhen: (previous, current) => !previous.toDay.isSameDay(current.toDay),
+          builder: (context, state) {
+            return TextButton(
+              style: ButtonStyle(padding: MaterialStateProperty.all(EdgeInsets.all(0))),
+              onPressed: () => _chooseDay(context, state),
+              child: Text(
+                  "${DateFormat('E, d MMMM, y', Localizations.localeOf(context).languageCode).format(state.toDay)}",
+                  style: PlanPageConstant.of(context).textFieldStyle),
+            );
+          },
+        ),
+      ),
+      trailing: Padding(
+        padding: EdgeInsets.only(right: 10),
+        child: BlocBuilder<PlanBloc, PlanState>(
+          buildWhen: (previous, current) => !previous.toDay.isSameTime(current.toDay),
+          builder: (context, state) {
+            return TextButton(
+              onPressed: () => _chooseTime(context, state),
+              child: Text(
+                "${DateFormat.Hm().format(state.toDay)}",
+                style: PlanPageConstant.of(context).textFieldStyle,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  final Function _chooseDay = (BuildContext context, PlanState state) async {
+    DateTime? newDate = await showDatePicker(
+      context: context,
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+      initialDate: state.toDay,
+    );
+
+    if (newDate != null) {
+      newDate = DateTime(
+        newDate.year,
+        newDate.month,
+        newDate.day,
+        state.toDay.hour,
+        state.toDay.minute,
+      );
+
+      context.read<PlanBloc>().add(PlanFromDateChanged(newDate));
+    }
+  };
+
+  final Function _chooseTime = (BuildContext context, PlanState state) async {
+    TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(state.toDay),
+    );
+
+    if (newTime != null) {
+      DateTime newDate = DateTime(
+        state.toDay.year,
+        state.toDay.month,
+        state.toDay.day,
+        newTime.hour,
+        newTime.minute,
+      );
+
+      context.read<PlanBloc>().add(PlanFromDateChanged(newDate));
+    }
+  };
 }
 
 //#endregion
@@ -150,51 +225,80 @@ class _DatePickerState extends State<DatePicker> {
 //#region Repeat
 
 class Repeat extends StatefulWidget {
+  const Repeat({Key? key}) : super(key: key);
+
   @override
   _RepeatState createState() => _RepeatState();
 }
 
-// ignore: camel_case_types
 class _RepeatState extends State<Repeat> {
-  late String value = 'Không lặp lại';
-  List _listItem = [
-    'Không lặp lại',
-    'Hàng ngày',
-    'Hàng tuần',
-    'Hàng tháng',
-    'Hàng năm',
-    'Tuỳ chỉnh...'
-  ];
+  static List<PlanRepeat> _listItem = PlanRepeat.values.toList();
 
   @override
   Widget build(BuildContext context) {
-    return PlanPageCustomListTile(
-      leading: Icon(Icons.refresh),
-      title: CommonPadding(
-        child: DropdownButton(
-            dropdownColor: Colors.grey,
-            style: TextStyle(color: Colors.black, fontSize: 22.0),
-            elevation: 5,
-            icon: new Icon(Icons.arrow_drop_down),
-            iconSize: 36.0,
-            isExpanded: true,
-            value: value,
-            onChanged: (newValue) {
-              setState(() {
-                value = newValue.toString();
-              });
-            },
-            items: _listItem.map((option) {
-              return DropdownMenuItem(
-                value: option,
-                child: Text(
-                  option,
-                  style: PlanPageConstant.of(context).textFieldStyle,
-                ),
-              );
-            }).toList()),
-      ),
+    return BlocBuilder<PlanBloc, PlanState>(
+      buildWhen: (previous, current) => previous.repeat != current.repeat,
+      builder: (context, state) {
+        return PlanPageCustomListTile(
+          leading: Icon(Icons.refresh),
+          title: Text(
+            state.repeat.string,
+            style: PlanPageConstant.of(context).textFieldStyle,
+          ),
+          onTap: () async {
+            await showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  contentPadding: EdgeInsets.symmetric(vertical: 10),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: listRadioItem(state),
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
     );
+  }
+
+  List<Widget> listRadioItem(PlanState state) {
+    List<Widget> widgets = [];
+
+    for (int i = 0; i < _listItem.length; i++) {
+      widgets.add(InkWell(
+        onTap: () => _onTap(_listItem[i]),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Row(
+            children: <Widget>[
+              Radio<PlanRepeat>(
+                value: _listItem[i],
+                groupValue: state.repeat,
+                onChanged: (planRepeat) => _onTap(planRepeat),
+              ),
+              SizedBox(width: 5),
+              Text(
+                _listItem[i].string,
+                style: PlanPageConstant.of(context).textFieldStyle,
+              )
+            ],
+          ),
+        ),
+      ));
+    }
+
+    return widgets;
+  }
+
+  void _onTap(PlanRepeat? planRepeat) {
+    if (planRepeat != null) {
+      context.read<PlanBloc>().add(PlanRepeatChanged(planRepeat));
+    }
+
+    Navigator.of(context).pop();
   }
 }
 
