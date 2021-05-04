b70c7cdca477ce584ee52c90b7ba828734c651cd
An Nguyen <annguyen.it.utc@gmail.com>
2021-05-04 22:21:28 +0700

    Implement BLoC for plan page title

--- lib/plan/view/local_widgets/title.dart
+++ lib/plan/view/local_widgets/title.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:app_qldt/plan/bloc/plan_bloc.dart';
 
 class PlanPageTitle extends StatelessWidget {
   final _textStyle = TextStyle(fontSize: 25);
@@ -7,13 +10,19 @@ class PlanPageTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 60),
-      child: TextField(
-        style: _textStyle,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: 'Thêm tiêu đề',
-          hintStyle: _textStyle,
-        ),
+      child: BlocBuilder<PlanBloc, PlanState>(
+        buildWhen: (previous, current) => previous.title != current.title,
+        builder: (context, state) {
+          return TextField(
+            style: _textStyle,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Thêm tiêu đề',
+              hintStyle: _textStyle,
+            ),
+            onChanged: (title) => context.read<PlanBloc>().add(PlanTitleChanged(title)),
+          );
+        },
       ),
     );
   }
