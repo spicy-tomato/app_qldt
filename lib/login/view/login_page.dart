4d232545066c5d0c8c546343144ce669aaa27626
An Nguyen <annguyen.it.utc@gmail.com>
2021-05-29 18:27:15 +0700

    Feat: Rename background image

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -91,7 +91,7 @@ class BackScreen extends StatelessWidget {
       duration: Duration(milliseconds: 1000),
       decoration: new BoxDecoration(
         image: new DecorationImage(
-          image: new AssetImage("images/backgr.gif"),
+          image: new AssetImage("images/background.gif"),
           fit: BoxFit.fill,
         ),
       ),
