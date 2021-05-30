b76b784117366238fb9d170a9328fd119052b15f
An Nguyen <annguyen.it.utc@gmail.com>
2021-05-30 22:09:19 +0700

    Feat: Make some field of ScoreModel nullable

--- lib/_models/score_model.dart
+++ lib/_models/score_model.dart
@@ -2,19 +2,19 @@ class ScoreModel {
   final String semester;
   final String moduleName;
   final int credit;
-  final String evaluation;
-  final double processScore;
-  final double testScore;
-  final double theoreticalScore;
+  final String? evaluation;
+  final double? processScore;
+  final double? testScore;
+  final double? theoreticalScore;
 
   ScoreModel({
     required this.semester,
     required this.moduleName,
     required this.credit,
-    required this.evaluation,
-    required this.processScore,
-    required this.testScore,
-    required this.theoreticalScore,
+    this.evaluation,
+    this.processScore,
+    this.testScore,
+    this.theoreticalScore,
   });
 
   factory ScoreModel.fromJson(Map<String, dynamic> json) {
@@ -23,9 +23,9 @@ class ScoreModel {
       semester: json['Semester'],
       credit: json['Credit'],
       evaluation: json['Evaluation'],
-      processScore: json['Process_Score'].toDouble(),
-      testScore: json['Test_Score'].toDouble(),
-      theoreticalScore: json['Theoretical_Score'].toDouble(),
+      processScore: json['Process_Score'] == null ? null : json['Process_Score'].toDouble(),
+      testScore: json['Test_Score'] == null ? null : json['Test_Score'].toDouble(),
+      theoreticalScore: json['Theoretical_Score'] == null ? null : json['Theoretical_Score'].toDouble(),
     );
   }
 
@@ -34,10 +34,10 @@ class ScoreModel {
       moduleName: map['module_name'],
       semester: map['semester'],
       credit: map['credit'],
-      evaluation: map['evaluation'],
-      processScore: map['process_score'],
-      testScore: map['test_score'],
-      theoreticalScore: map['theoretical_score'],
+      evaluation: map['evaluation'] == null ? null : map['evaluation'],
+      processScore: map['process_score'] == null ? null : map['process_score'],
+      testScore: map['test_score'] == null ? null : map['test_score'],
+      theoreticalScore: map['theoretical_score'] == null ? null : map['theoretical_score'],
     );
   }
 
