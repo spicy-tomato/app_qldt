0d6891088b824fe0819c32d735616031d3e15b03
An Nguyen <annguyen.it.utc@gmail.com>
2021-07-31 00:33:51 +0700

    Fix: Fixed #16

--- lib/login/view/local_widgets/password_input.dart
+++ lib/login/view/local_widgets/password_input.dart
@@ -5,10 +5,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:app_qldt/login/bloc/login_bloc.dart';
 import 'style/style.dart';
 
-class PasswordInput extends StatelessWidget {
+class PasswordInput extends StatefulWidget {
   final FocusNode focusNode;
 
   const PasswordInput({Key? key, required this.focusNode}) : super(key: key);
+
+  @override
+  _PasswordInputState createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  late FocusNode passwordFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    passwordFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +37,12 @@ class PasswordInput extends StatelessWidget {
         children: <Widget>[
           BlocBuilder<LoginBloc, LoginState>(
             buildWhen: (previous, current) {
-              return previous.password != current.password ||
-                  previous.hidePassword != current.hidePassword;
+              return previous.password != current.password || previous.hidePassword != current.hidePassword;
             },
             builder: (context, state) {
               return TextField(
                 key: const Key('loginForm_passwordInput_textField'),
-                onChanged: (password) =>
-                    context.read<LoginBloc>().add(LoginPasswordChanged(password)),
+                onChanged: (password) => context.read<LoginBloc>().add(LoginPasswordChanged(password)),
                 style: const FormTextStyle(),
                 obscureText: state.hidePassword,
                 decoration: InputDecoration(
@@ -33,15 +50,19 @@ class PasswordInput extends StatelessWidget {
                   errorText: state.password.invalid ? 'Hãy nhập mật khẩu' : null,
                   contentPadding: const EdgeInsets.only(right: 48),
                 ),
+                focusNode: passwordFocusNode,
                 textInputAction: TextInputAction.done,
-                onSubmitted: (_) => focusNode.unfocus(),
+                onSubmitted: (_) => widget.focusNode.unfocus(),
               );
             },
           ),
           IconButton(
             icon: const Icon(Icons.remove_red_eye),
             iconSize: 20,
-            onPressed: () => context.read<LoginBloc>().add(LoginPasswordVisibleChanged()),
+            onPressed: () {
+              passwordFocusNode.requestFocus();
+              context.read<LoginBloc>().add(LoginPasswordVisibleChanged());
+            },
           )
         ],
       ),
