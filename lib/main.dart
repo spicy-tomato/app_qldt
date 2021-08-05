4ed0ee9881ebad7dd8706f9e7aa8495492513d76
An Nguyen <annguyen.it.utc@gmail.com>
2021-08-05 12:07:03 +0700

    Fix: Fixed #25

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
@@ -11,6 +12,7 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp();
 
   // var _clientId = new ClientId(Secret.getId(), '');
